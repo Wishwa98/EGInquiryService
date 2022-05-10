8f5cdd8cef58badb1b75eb7997a21a95b7ed1274
Wishwa98 <wishwa5020@gmail.com>
2022-05-11 03:36:14 +0530

    Database Connection added

--- InquiryService/InquiryService/src/main/java/com/Inquiry.java
+++ InquiryService/InquiryService/src/main/java/com/Inquiry.java
@@ -1,5 +1,24 @@
 package com;
+import java.sql.*;
+
 
 public class Inquiry {
 
+	//A common method to connect to the DataBase
+		private Connection connect()
+		 {
+		 Connection con = null;
+		 try
+		 {
+		 Class.forName("com.mysql.jdbc.Driver");
+
+		 //Details of the database
+		 con = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/electrogriddb", "root", "");
+		 }
+		 catch (Exception e)
+		 {e.printStackTrace();}
+		 return con;
+		 } 
+	
+	
 }
